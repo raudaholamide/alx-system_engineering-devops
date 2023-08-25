22102362551f82d54abdde3cdf69004c5b019a41
raudaholamide <123153259+raudaholamide@users.noreply.github.com>
2023-08-26 00:19:04 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 # Create a file in /tmp using puppet
 file {'holberton':
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   mode    =>'0744',
   owner   => 'www-data',
   group   => 'www-data',

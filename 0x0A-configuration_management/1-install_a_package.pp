146d764eda0407ec9b5c0e136ba3e4f7985894b1
raudaholamide <123153259+raudaholamide@users.noreply.github.com>
2023-08-26 01:20:41 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install flask.pp
-
-package { 'flask':
+#!/usr/bin/pup
+# Install a specific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3'
 }
